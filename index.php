b964a6ecb05d16bc3fb8b753399dd3adbc016cb2
starman360 <anmol.modur@gmail.com>
2015-12-21 18:49:26 -0500

    First commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<!--index.php-->
+<!--body-->
+<?php include 'php/header.php' ?>
+ Hello
+
+
+<?php include 'php/footer.php' ?>
---
+++ php/header.php
@@ -0,0 +1,17 @@
+<?php
+// header.inc
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>RIT BAJA</title>
+        
+        <!--CSS Stylesheet-->
+        <link herf="style/style.css" rel="stylesheet" type="text/css">
+        
+        <!--Bootstrap Stylesheet-->
+        <link herf="style/bootstrap.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
