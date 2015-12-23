71ea61505c05c58017ba4c39cc8bd06c0fab2300
starman360 <anmol.modur@gmail.com>
2015-12-23 17:45:42 -0500

    Added Nav bar

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <!--index.php-->
 <!--body-->
 <?php include 'php/header.php' ?>
- Hello
-There is so much to be said!
 
 <?php include 'php/footer.php' ?>
--- php/header.php
+++ php/header.php
@@ -6,12 +6,20 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>RIT BAJA</title>
-        
-        <!--CSS Stylesheet-->
-        <link herf="style/style.css" rel="stylesheet" type="text/css">
-        
         <!--Bootstrap Stylesheet-->
-        <link herf="style/bootstrap.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+        <!--CSS Stylesheet-->
+        <link herf="/style.css" rel="stylesheet" type="text/css">
     </head>
     <body>
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+                <div class="navbar-header"> 
+                    <a href="#" class="navbar-brand">
+                        <img id="logo" src="images/Baja_logo.png" alt="Baja">
+                    </a>
+                </div>
+            </div>
+        </nav>
